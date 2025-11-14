879f1326a02ad3add2275f730aecd92e2a9cd217
Juan Daniel Ferreira Ibanez <dev.juan.ibanez@gmail.com>
2025-11-14 15:44:42 +0000

    update

--- desafio-portfolio/assets/js/api.js
+++ desafio-portfolio/assets/js/api.js
@@ -1,6 +1,7 @@
 
 async function fetchProfileData() {
-    const url = 'https://github.com/dev-juan-ibanez/formacao-javascript-developer-dio/blob/main/desafio-portfolio/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/dev-juan-ibanez/formacao-javascript-developer-dio/main/desafio-portfolio/data/profile.json';
+
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
